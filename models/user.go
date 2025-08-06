835a339c7744057cba3d0cb7c94929cfaa411263
Wagner Esser <waesser@gmail.com>
2025-08-06 13:26:02 -0300

    feat: enhance password validation to require special characters

--- models/user.go
+++ models/user.go
@@ -4,11 +4,11 @@ import "github.com/google/uuid"
 
 type User struct {
 	ID          uuid.UUID `json:"id"`
-	Age         int16     `json:"age" validate:"gte=0,lte=130"`          // example of age validation
+	Age         int16     `json:"age" validate:"gte=0,lte=130"`
 	PhoneNumber string    `json:"phone_number" validate:"required,e164"` // e164 = international phone format
 	Email       string    `json:"email" validate:"required,email,min=4,max=100"`
-	Address     Address   `json:"address" validate:"required"`                  // dive = validate internal fields
-	Password    string    `json:"password,omitempty" validate:"required,min=6"` // example
+	Address     Address   `json:"address" validate:"required"` // dive = validate internal fields
+	Password    string    `json:"password,omitempty" validate:"required,min=6,containsany=!@#$%&*"`
 }
 
 type UserResponse struct {
